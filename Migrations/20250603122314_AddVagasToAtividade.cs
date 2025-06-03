2450227c051811b5f37c76359d2abad2095bdd3f
DrsFrazao <danielfrazao0811@gmail.com>
2025-06-03 09:32:59 -0300

    Atualizar a tela de formulário para mostrar as vagas disponíveis ao lado do nome da atividade

---
+++ Migrations/20250603122314_AddVagasToAtividade.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteConhecimentoDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVagasToAtividade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Atividade.cs
+++ Models/Atividade.cs
@@ -2,15 +2,40 @@
 
 namespace TesteConhecimentoDb.Models
 {
+    /* public class Atividade
+     {
+         [Key]
+         public int CodAtv { get; set; }
+
+         public string DescAtv { get; set; } = string.Empty;
+         public int Vagas { get; set; }
+         public decimal Preco { get; set; }
+
+         public virtual ICollection<AxParticipanteAtividade> AxParticipanteAtividades { get; set; } = new List<AxParticipanteAtividade>();
+     }*/
+
+
+    using System.ComponentModel.DataAnnotations;
+
     public class Atividade
     {
-        [Key]
+        [Key] // <- ESSENCIAL
         public int CodAtv { get; set; }
 
         public string DescAtv { get; set; } = string.Empty;
-        public int Vagas { get; set; }
+
         public decimal Preco { get; set; }
+
+        public int Vagas { get; set; } // <- novo campo
 
         public virtual ICollection<AxParticipanteAtividade> AxParticipanteAtividades { get; set; } = new List<AxParticipanteAtividade>();
     }
+
+
+
+
+
+
+
+
 }
